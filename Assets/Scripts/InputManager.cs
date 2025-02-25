642566b7a91424e1f0d7e8f50ec35c7ec8e91389
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-02-25 12:19:36 -0800

    added movement to player

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,12 +6,37 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent onSpacePressed = new UnityEvent();
-    public UnityEvent onAPressed = new UnityEvent();
-    public UnityEvent onDPress = new UnityEvent();
-    public UnityEvent onWPress = new UnityEvent();
-    public UnityEvent onSPress = new UnityEvent();
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onSpacePressed?.Invoke();
+        }
         
+
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        OnMove?.Invoke(inputVector);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void MovePlayer(Vector2 direction)
     {
-        
-    }
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }       
 }
