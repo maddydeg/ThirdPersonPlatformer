7dc0a7bd0aec4605bc264b8fefa4caeebcac4a8d
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-03-02 10:00:06 -0800

    Added coin prefab, and added scripting to rotate coin

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, rotateSpeed, 0);
+    }
+}
